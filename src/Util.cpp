934909fbb2c4adcfe6e12b00befb3e86b6cae02e
Sander Valcke <Sander.Valcke@bricsys.com>
2020-02-05 19:34:40 +0100

    More keywords for C++ syntax highlighting

--- src/Util.cpp
+++ src/Util.cpp
@@ -37,8 +37,8 @@ QString Util::highlightPlainText(QString plainText, QString fontFamily, int font
 // todo!sv reimplement using QSyntaxHighlighter
 QString Util::highlightRichText(QString richText)
 {
-    auto keywords = {"int", "float", "double", "long", "string", "char", "auto", "const",
-                     "std",
+    auto keywords = {"int", "float", "double", "long", "char", "auto", "const", "void",
+                     "struct", "class", "public", "template", "this", "typename",
                      "return", "if", "then", "else", "while", "break", "include", "#pragma once"};
 
     // Whole next code should be replaced by QSyntaxHighlighter or even better a library that does that
